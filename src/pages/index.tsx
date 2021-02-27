fc4e792d8a2e7241a2a1d2ba774e32f70648f714
Jozias Martini <jozias.martini@gmail.com>
2021-02-27 18:38:35 -0300

    Create challenge box component and context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from '../components/ChallengeBox'
 
 export default function Home() {
   return (
@@ -20,7 +21,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
 		</div>
